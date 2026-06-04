7aa65389773dcaa7d4c71093e7c9e3d5ba067e98
akalizateta <akalizatetakelcy@proton.me>
2026-06-04 11:32:39 +0200

    second-commit

--- server.js
+++ server.js
@@ -37,6 +37,9 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "Student Chat Backend is live 🚀" });
+});
 // Routes
 app.use('/api/auth', authRouter);
 
